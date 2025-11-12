4df47542eb111fc1acc4640e306af039aa58f20e
xinger0804 <lixinge0804@whu.edu.cn>
2025-11-12 20:20:05 +0800

    fix:标签闭合问题

--- frontend/src/pages/oj/views/problem/Problem.vue
+++ frontend/src/pages/oj/views/problem/Problem.vue
@@ -149,7 +149,6 @@
             <p>{{$t('m.Memory_Limit')}}</p>
             <p>{{problem.memory_limit}}MB</p></li>
           <li>
-          <li>
             <p>{{$t('m.IOMode')}}</p>
             <p>{{problem.io_mode.io_mode}}</p>
           </li>
